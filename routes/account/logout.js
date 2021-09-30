6bba72ac985d0be3700e9d061e8777b7d5cc5ed3
AzureAD\TerryTan <terry.tan@flex-solver.com.sg>
2021-09-30 14:35:46 +0800

    Add logout api

---
+++ routes/account/logout.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const rb = require("@flexsolver/flexrb");
+const storage = require("node-persist");
+
+router.post("/", async (req, res, next) => {
+    await storage.clear();
+    res.send(rb.build("Logout successful!"));
+});
+
+module.exports = router;
