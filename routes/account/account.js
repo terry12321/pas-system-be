8edfbfa75b12797eb253cfec357f35e02de71397
AzureAD\TerryTan <terry.tan@flex-solver.com.sg>
2021-10-02 13:07:19 +0800

    remove routes and put them into account.js

--- routes/account/account.js
+++ routes/account/account.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
 const formatError = require("../../common/error_formatter");
 const authenticate = require("../../authenticate");
 const rb = require("@flexsolver/flexrb");
-const storage = require("node-persist");
 const qp = require("@flexsolver/flexqp2");
+const bcrypt = require("bcrypt");
+const storage = require("node-persist");
 
-router.post("/", async (req, res, next) => {
+router.post("/relogin", async (req, res, next) => {
     const user = req.user;
     let get_updated_user;
     let token = req.headers.authorization;
@@ -31,8 +31,32 @@ router.post("/", async (req, res, next) => {
 
         res.json(rb.build(result));
     } catch (error) {
-        res.status(error.status).json(formatError(401, error.message));
+        res.status(500).json(formatError(500, error.message));
     }
+});
+
+router.post("/change_password", async (req, res, next) => {
+    let con;
+    try {
+        con = await qp.connectWithTbegin();
+        const body = req.body;
+        const user = req.user;
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(body.password, salt);
+        const result = await qp.run(`UPDATE account SET password = ? WHERE id = ?`, [hash, user.id]);
+        await qp.commitAndCloseConnection(con);
+        res.send(rb.build(result, "Password changed successfully!"));
+    } catch (error) {
+        if (con) {
+            await qp.rollbackAndCloseConnection(con);
+        }
+        res.status(406).send(formatError(error.status, error.message));
+    }
+});
+
+router.post("/logout", async (req, res, next) => {
+    await storage.clear();
+    res.send(rb.build("Logout successful!"));
 });
 
 module.exports = router;
